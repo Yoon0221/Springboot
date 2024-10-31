18bab395aeedbeafdf5439bb4c0053dc63e83c53
Yoon0221 <108733746+Yoon0221@users.noreply.github.com>
2024-10-31 17:22:08 +0900

    test: cd_v1

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/yoon_1")
 public class UserController {
 
     @Autowired
